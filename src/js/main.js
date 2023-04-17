5920334430b9b01327ada404bfa837dd0d1d5fef
yastrb <uaalena202@gmail.com>
2023-04-17 17:38:50 +0300

    new commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+﻿const navToggle = document.querySelector("#nav-toggle");
+const close = document.querySelector("#close");
+const menu = document.querySelector("#menu-mobile");
+
+navToggle.addEventListener('click', function(){
+    menu.classList.remove("hidden")
+    menu.classList.add("fixed");
+    console.log("click")
+});
+close.addEventListener('click', function(){
+    menu.classList.remove("fixed")
+    menu.classList.add("hidden");
+    console.log("click")
+});
+
